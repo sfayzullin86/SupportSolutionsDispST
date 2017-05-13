b653f91e658f9cdb5a40e0b24bd930f74b2422b8
ljs <sfayzullin86@gmail.com>
2017-05-13 17:26:40 +0000

    First autogeneration

---
+++ webapp/Component.js
@@ -0,0 +1,9 @@
+jQuery.sap.declare("srh.comsupportsolutionsdispst.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("srh.comsupportsolutionsdispst.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
